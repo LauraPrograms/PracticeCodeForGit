ce5b36c2951a908c7db019fcd9e63447ebc95a4f
mattlaura <lhuiefiction@gmail.com>
2019-01-15 10:53:02 -0500

    this is for my practice code, this does if statements

---
+++ GitHubUp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input;
+            Console.WriteLine("Is this a file to be uploaded to Git? (y/n)");
+            input = Console.ReadLine();
+            input = input.ToLower();
+            if (input == "y")
+            {
+                Console.WriteLine("Please upload it to GitHub.");
+            }
+            else
+            {
+                Console.WriteLine("Okay, have a good day");
+            }
+
+            Console.ReadLine();
+            //try adding a try catch code to improve this
+        }
+
+    }
+}
